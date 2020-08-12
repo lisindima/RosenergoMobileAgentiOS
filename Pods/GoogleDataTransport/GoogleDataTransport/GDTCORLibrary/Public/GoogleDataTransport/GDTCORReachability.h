8702a909e054669a57bb886f7a01652a5d97084c
lisindima <lisindima1996@gmail.com>
2020-08-12 09:34:23 +0700

    Update GDTCORReachability.h

--- Pods/GoogleDataTransport/GoogleDataTransport/GDTCORLibrary/Public/GoogleDataTransport/GDTCORReachability.h
+++ Pods/GoogleDataTransport/GoogleDataTransport/GDTCORLibrary/Public/GoogleDataTransport/GDTCORReachability.h
@@ -22,6 +22,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /** This class helps determine upload conditions by determining connectivity. */
 @interface GDTCORReachability : NSObject
+
 /** The current set flags indicating network conditions */
 + (GDTCORNetworkReachabilityFlags)currentFlags;
 
