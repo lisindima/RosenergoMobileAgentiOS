021a677ccf353261bc003979f6490f2b9b1f516f
lisindima <lisindima1996@gmail.com>
2020-08-26 15:31:44 +0700

    Исправление ошибок

--- RosenergoMobileAgentiOS/LinkDetails.swift
+++ RosenergoMobileAgentiOS/LinkDetails.swift
@@ -7,9 +7,9 @@
 //
 
 import Alamofire
-import AVKit
-import CoreLocation
-import MapKit
+#if !os(watchOS)
+    import AVKit
+#endif
 import SwiftUI
 import URLImage
 
@@ -20,11 +20,7 @@ struct LinkDetails: View {
 
     @Binding var inspectionID: String?
 
-    @State private var address: String?
     @State private var inspection: Inspections?
-    @State private var loadAddress: Bool = false
-    @State private var pins: [Pin] = []
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
 
     var scale: CGFloat {
         #if os(watchOS)
@@ -53,7 +49,6 @@ struct LinkDetails: View {
                 switch response {
                 case let .success(value):
                     inspection = value
-                    loadAddress = true
                 case let .failure(error):
                     presentationMode.wrappedValue.dismiss()
                     print(error)
@@ -63,149 +58,127 @@ struct LinkDetails: View {
 
     var body: some View {
         NavigationView {
-            Form {
-                if inspection?.photos != nil {
-                    Section(header: Text("Фотографии").fontWeight(.bold)) {
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            LazyHStack {
-                                ForEach(inspection!.photos, id: \.id) { photo in
-                                    NavigationLink(destination: ImageDetail(id: photo.id, photos: inspection!.photos)) {
-                                        URLImage(URL(string: photo.path)!, delay: 0.25, processors: [Resize(size: CGSize(width: size, height: size), scale: scale)], placeholder: { _ in
-                                            ProgressView()
-                                        }, content: {
-                                            $0.image
-                                                .resizable()
-                                        })
-                                            .cornerRadius(8)
-                                            .frame(width: CGFloat(size), height: CGFloat(size))
-                                    }.buttonStyle(PlainButtonStyle())
-                                }
-                            }.padding(.vertical, 8)
+            if inspection == nil {
+                ProgressView("Загрузка")
+                    .onAppear(perform: getInspections)
+            } else {
+                Form {
+                    if !inspection!.photos.isEmpty {
+                        Section(header: Text("Фотографии").fontWeight(.bold)) {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                LazyHStack {
+                                    ForEach(inspection!.photos, id: \.id) { photo in
+                                        NavigationLink(destination: ImageDetail(id: photo.id, photos: inspection!.photos)) {
+                                            URLImage(URL(string: photo.path)!, delay: 0.25, processors: [Resize(size: CGSize(width: size, height: size), scale: scale)], placeholder: { _ in
+                                                ProgressView()
+                                            }, content: {
+                                                $0.image
+                                                    .resizable()
+                                            })
+                                                .cornerRadius(8)
+                                                .frame(width: CGFloat(size), height: CGFloat(size))
+                                        }.buttonStyle(PlainButtonStyle())
+                                    }
+                                }.padding(.vertical, 8)
+                            }
                         }
                     }
-                }
-                #if !os(watchOS)
-                    if inspection?.video != nil {
-                        Section(header: Text("Видео").fontWeight(.bold)) {
-                            VideoPlayer(player: AVPlayer(url: URL(string: inspection!.video!)!))
-                                .frame(height: 200)
-                                .cornerRadius(8)
-                                .padding(.vertical, 8)
+                    #if !os(watchOS)
+                        if inspection?.video != nil {
+                            Section(header: Text("Видео").fontWeight(.bold)) {
+                                VideoPlayer(player: AVPlayer(url: URL(string: inspection!.video!)!))
+                                    .frame(height: 200)
+                                    .cornerRadius(8)
+                                    .padding(.vertical, 8)
+                            }
                         }
+                    #endif
+                    Section(header: Text("Дата загрузки осмотра").fontWeight(.bold)) {
+                        SectionItem(
+                            imageName: "timer",
+                            imageColor: .rosenergo,
+                            title: inspection?.createdAt.dataInspection(local: false)
+                        )
                     }
-                #endif
-                Section(header: Text("Дата загрузки осмотра").fontWeight(.bold)) {
-                    SectionItem(
-                        imageName: "timer",
-                        imageColor: .rosenergo,
-                        title: inspection?.createdAt.dataInspection(local: false)
-                    )
-                }
-                Section(header: Text(inspection?.carModel2 != nil ? "Первый автомобиль" : "Информация").fontWeight(.bold)) {
-                    SectionItem(
-                        imageName: "doc.plaintext",
-                        imageColor: .rosenergo,
-                        subTitle: "Страховой полис",
-                        title: inspection?.insuranceContractNumber
-                    )
-                    SectionItem(
-                        imageName: "car",
-                        imageColor: .rosenergo,
-                        subTitle: "Модель автомобиля",
-                        title: inspection?.carModel
-                    )
-                    SectionItem(
-                        imageName: "rectangle",
-                        imageColor: .rosenergo,
-                        subTitle: "Регистрационный номер",
-                        title: inspection?.carRegNumber
-                    )
-                    SectionItem(
-                        imageName: "v.circle",
-                        imageColor: .rosenergo,
-                        subTitle: "VIN",
-                        title: inspection?.carVin
-                    )
-                    SectionItem(
-                        imageName: "textformat.123",
-                        imageColor: .rosenergo,
-                        subTitle: "Номер кузова",
-                        title: inspection?.carBodyNumber
-                    )
-                }
-                if inspection?.carModel2 != nil {
-                    Section(header: Text("Второй автомобиль").fontWeight(.bold)) {
+                    Section(header: Text(inspection?.carModel2 != nil ? "Первый автомобиль" : "Информация").fontWeight(.bold)) {
                         SectionItem(
                             imageName: "doc.plaintext",
                             imageColor: .rosenergo,
                             subTitle: "Страховой полис",
-                            title: inspection?.insuranceContractNumber2
+                            title: inspection?.insuranceContractNumber
                         )
                         SectionItem(
                             imageName: "car",
                             imageColor: .rosenergo,
                             subTitle: "Модель автомобиля",
-                            title: inspection?.carModel2
+                            title: inspection?.carModel
                         )
                         SectionItem(
                             imageName: "rectangle",
                             imageColor: .rosenergo,
                             subTitle: "Регистрационный номер",
-                            title: inspection?.carRegNumber2
+                            title: inspection?.carRegNumber
                         )
                         SectionItem(
                             imageName: "v.circle",
                             imageColor: .rosenergo,
                             subTitle: "VIN",
-                            title: inspection?.carVin2
+                            title: inspection?.carVin
                         )
                         SectionItem(
                             imageName: "textformat.123",
                             imageColor: .rosenergo,
                             subTitle: "Номер кузова",
-                            title: inspection?.carBodyNumber2
+                            title: inspection?.carBodyNumber
                         )
                     }
-                }
-                Section(header: Text("Место проведения осмотра").fontWeight(.bold), footer: Text("Для того, чтобы открыть это местоположение в приложение карт, нажмите на адрес.")) {
-                    SectionLink(
-                        imageName: "map",
-                        imageColor: .rosenergo,
-                        title: address,
-                        titleColor: .primary,
-                        destination: URL(string: "yandexmaps://maps.yandex.ru/?pt=\(inspection?.longitude ?? 0.0),\(inspection?.latitude ?? 0.0)")!
-                    )
-                    .disabled(inspection == nil ? true : false)
-                    Map(coordinateRegion: $region, annotationItems: pins) { pin in
-                        MapMarker(coordinate: pin.coordinate, tint: .rosenergo)
+                    if inspection?.carModel2 != nil {
+                        Section(header: Text("Второй автомобиль").fontWeight(.bold)) {
+                            SectionItem(
+                                imageName: "doc.plaintext",
+                                imageColor: .rosenergo,
+                                subTitle: "Страховой полис",
+                                title: inspection?.insuranceContractNumber2
+                            )
+                            SectionItem(
+                                imageName: "car",
+                                imageColor: .rosenergo,
+                                subTitle: "Модель автомобиля",
+                                title: inspection?.carModel2
+                            )
+                            SectionItem(
+                                imageName: "rectangle",
+                                imageColor: .rosenergo,
+                                subTitle: "Регистрационный номер",
+                                title: inspection?.carRegNumber2
+                            )
+                            SectionItem(
+                                imageName: "v.circle",
+                                imageColor: .rosenergo,
+                                subTitle: "VIN",
+                                title: inspection?.carVin2
+                            )
+                            SectionItem(
+                                imageName: "textformat.123",
+                                imageColor: .rosenergo,
+                                subTitle: "Номер кузова",
+                                title: inspection?.carBodyNumber2
+                            )
+                        }
                     }
-                    .frame(height: 200)
-                    .cornerRadius(8)
-                    .padding(.vertical)
+                    Section(header: Text("Место проведения осмотра").fontWeight(.bold), footer: Text("Для того, чтобы открыть это местоположение в приложение карт, нажмите на адрес.")) {
+                        MapView(latitude: inspection!.latitude, longitude: inspection!.longitude)
+                    }
+                }
+                .navigationTitle("Осмотр: \(inspection!.id)")
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button(action: { presentationMode.wrappedValue.dismiss() }) {
+                            Text("Закрыть")
+                        }
+                    }
                 }
             }
-            .navigationTitle(inspection?.id != nil ? "Осмотр: \(inspection!.id)" : "Загрузка")
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Button(action: { presentationMode.wrappedValue.dismiss() }) {
-                        Text("Закрыть")
-                    }
-                }
-            }
-            .onChange(of: loadAddress) { _ in
-                region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: inspection!.latitude, longitude: inspection!.longitude), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-                pins.append(Pin(coordinate: .init(latitude: inspection!.latitude, longitude: inspection!.longitude)))
-                let location = CLLocation(latitude: inspection!.latitude, longitude: inspection!.longitude)
-                location.geocode { placemark, error in
-                    if let error = error {
-                        print(error)
-                        return
-                    } else if let placemark = placemark?.first {
-                        address = "\(placemark.country ?? ""), \(placemark.administrativeArea ?? ""), \(placemark.locality ?? ""), \(placemark.name ?? "")"
-                    }
-                }
-            }
-            .onAppear(perform: getInspections)
         }
     }
 }
