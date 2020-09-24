6abd23f4bd071d0c1260c4e00d747d2612e22a00
lisindima <lisindima1996@gmail.com>
2020-09-24 16:01:58 +0700

    CoreData в виджете

--- RosenergoMobileAgentWidget/RosenergoMobileAgentWidget.swift
+++ RosenergoMobileAgentWidget/RosenergoMobileAgentWidget.swift
@@ -6,20 +6,27 @@
 //  Copyright © 2020 Дмитрий Лисин. All rights reserved.
 //
 
-import WidgetKit
+import CoreData
 import SwiftUI
+import WidgetKit
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
+    var managedObjectContext: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext) {
+        managedObjectContext = context
+    }
+    
+    func placeholder(in _: Context) -> SimpleEntry {
         SimpleEntry(date: Date())
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+    func getSnapshot(in _: Context, completion: @escaping (SimpleEntry) -> Void) {
         let entry = SimpleEntry(date: Date())
         completion(entry)
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    func getTimeline(in _: Context, completion: @escaping (Timeline<Entry>) -> Void) {
         var entries: [SimpleEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
@@ -39,9 +46,9 @@ struct SimpleEntry: TimelineEntry {
     let date: Date
 }
 
-struct RosenergoMobileAgentWidgetEntryView : View {
-    var entry: Provider.Entry
-
+struct SystemSmall: View {
+    var inspections: Int
+    
     var body: some View {
         ZStack {
             Color.blue
@@ -49,24 +56,39 @@ struct RosenergoMobileAgentWidgetEntryView : View {
             VStack(alignment: .leading) {
                 Text("Осмотры")
                     .fontWeight(.bold)
-                    .padding(.bottom, 1)
-                    .foregroundColor(.white)
+                Text("Не отправленные")
+                    .fontWeight(.semibold)
+                    .font(.caption2)
+                Divider()
                 Text("Осмотры не отправлены на сервер")
-                    .font(.system(size: 13))
-                    .foregroundColor(.white)
-                    .padding(.bottom, 6)
+                    .font(.caption2)
                 Spacer()
                 HStack {
                     Image(systemName: "tray.circle.fill")
                         .imageScale(.large)
                     Spacer()
-                    Text("\(4) осмотра")
+                    Text("\(inspections) осмотра")
                         .fontWeight(.bold)
                         .font(.caption)
                 }
-                .foregroundColor(.white)
             }
+            .foregroundColor(.white)
             .padding()
+        }
+    }
+}
+
+struct RosenergoMobileAgentWidgetEntryView: View {
+    @Environment(\.widgetFamily) private var widgetFamily
+    
+    var entry: Provider.Entry
+    
+    var body: some View {
+        switch widgetFamily {
+        case .systemSmall:
+            SystemSmall(inspections: 5)
+        default:
+            SystemSmall(inspections: 5)
         }
     }
 }
@@ -74,12 +96,14 @@ struct RosenergoMobileAgentWidgetEntryView : View {
 @main
 struct RosenergoMobileAgentWidget: Widget {
     let kind: String = "RosenergoMobileAgentWidget"
-
+    
+    let persistenceController = PersistenceController.shared
+    
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+        StaticConfiguration(kind: kind, provider: Provider(context: persistenceController.container.viewContext)) { entry in
             RosenergoMobileAgentWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("Не отправленные осмотры")
+        .configurationDisplayName("Осмотры")
         .description("Виджет поможет вам не забыть отправить осмотр на сервер!")
         .supportedFamilies([.systemSmall])
     }
